b3c2eb556d009e9e3c73c48e1dd1c5ce1628836a
Ryan White <6309098+rythwh@users.noreply.github.com>
2025-06-03 13:12:16 +0200

    Added virus windows appearing and bouncing around.

---
+++ Assets/Scripts/BouncingWindow.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class BouncingWindow : MonoBehaviour
+{
+    private Vector2 velocity;
+    private RectTransform rectTransform;
+    private RectTransform bounceAreaRect;
+
+    void Start()
+    {
+        velocity = new Vector2(Random.Range(100f, 300f), Random.Range(100f, 300f)); // pixels per second
+        rectTransform = GetComponent<RectTransform>();
+
+        Events.SetSpeedMultiplier += SetSpeedMultiplier;
+    }
+
+    private void SetSpeedMultiplier(float speedMultiplier)
+    {
+        velocity *= speedMultiplier;
+    }
+
+    public void SetBounceArea(RectTransform bounceArea)
+    {
+        bounceAreaRect = bounceArea;
+    }
+
+    void Update()
+    {
+        if (!bounceAreaRect)
+        {
+            return;
+        }
+
+        Vector2 position = rectTransform.anchoredPosition;
+        Vector2 size = rectTransform.rect.size;
+        Vector2 bounceAreaSize = bounceAreaRect.rect.size;
+
+        // Move the window
+        position += velocity * Time.deltaTime;
+
+        // Check for collisions with edges and reflect direction
+        if (position.x < 0)
+        {
+            position.x = 0;
+            velocity.x *= -1;
+        }
+        else if (position.x + size.x > bounceAreaSize.x)
+        {
+            position.x = bounceAreaSize.x - size.x;
+            velocity.x *= -1;
+        }
+
+        if (position.y < 0)
+        {
+            position.y = 0;
+            velocity.y *= -1;
+        }
+        else if (position.y + size.y > bounceAreaSize.y)
+        {
+            position.y = bounceAreaSize.y - size.y;
+            velocity.y *= -1;
+        }
+
+        rectTransform.anchoredPosition = position;
+    }
+}
---
+++ Assets/Scripts/VirusWindowController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class VirusWindowController : MonoBehaviour
+{
+    [SerializeField] private RectTransform canvasRt;
+    [SerializeField] private GameObject virusWindowPrefab;
+    [SerializeField] private List<Sprite> virusSprites;
+    [SerializeField] private RectTransform bounceArea;
+
+    private void Start()
+    {
+        Events.OnShotBadAnimal += OnShotBadAnimal;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            Events.OnShotBadAnimal?.Invoke();
+        }
+    }
+
+    private void OnShotBadAnimal()
+    {
+        GameObject virusWindow = Instantiate(virusWindowPrefab, transform, false);
+        Sprite sprite = virusSprites[Random.Range(0, virusSprites.Count)];
+        virusWindow.GetComponent<Image>().sprite = sprite;
+        virusWindow.GetComponent<AspectRatioFitter>().aspectRatio = sprite.rect.width / sprite.rect.height;
+
+        RectTransform windowRt = (RectTransform)virusWindow.transform;
+        windowRt.anchoredPosition = new Vector3(Random.Range(0f, 1f) * canvasRt.rect.width, Random.Range(0f, 1f) * canvasRt.rect.height, 0);
+        windowRt.sizeDelta = new Vector2(Random.Range(200, 400), 0);
+
+        BouncingWindow bouncingWindow = virusWindow.GetComponent<BouncingWindow>();
+        bouncingWindow.SetBounceArea(bounceArea);
+    }
+}
